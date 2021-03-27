87c23a75d66c5911498fed00c1623ece20409fc0
sheeps1026 <sheeps1026@gmail.com>
2021-03-27 15:08:30 +0900

    JS: 복사 기능

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
 const bookmark = document.querySelector("#bookmark");
 
 const note = document.querySelector(".note");
+const noteCopy = document.querySelector(".note-copy");
 
+// Bookmark
 function bookmarkActive() {
   document.querySelector(".subsidebar").classList.toggle("active");
   document.querySelector(".note").classList.toggle("position");
@@ -21,3 +23,22 @@ function bookmarkFunction() {
 }
 
 bookmark.onclick = bookmarkFunction;
+
+// Copy
+function copy() {
+  const noteDesc = document.querySelector(".note-desc").innerText;
+
+  const tempDesc = document.createElement("textarea");
+  tempDesc.setAttribute("type", "text");
+
+  document.querySelector(".note-desc").appendChild(tempDesc);
+
+  tempDesc.value = noteDesc;
+
+  tempDesc.select();
+  document.execCommand("copy");
+
+  document.querySelector(".note-desc").removeChild(tempDesc);
+}
+
+noteCopy.onclick = copy;
