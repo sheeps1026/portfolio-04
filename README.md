b43ef92d880ed6be1906a4ccc1135a95113aac0b
sheeps1026 <sheeps1026@gmail.com>
2021-04-12 15:42:28 +0900

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # portfolio-04
+
+JavaScript를 이용한 간단한 메모앱을 만들고 있습니다.
+
+URL: [https://sheeps1026.github.io/portfolio-04/](https://sheeps1026.github.io/portfolio-04/)
+
+![20210331_012932](https://user-images.githubusercontent.com/55824972/114350823-b21b2f80-9ba4-11eb-82e6-ecd5413bf394.png)
+
+---
+
+포트폴리오 리스트
+
+- [반응형 웹사이트 1](https://sheeps1026.github.io/portfolio-02/)
+- [반응형 웹사이트 2](https://sheeps1026.github.io/portfolio-03/)
--- assets/js/notification.js
+++ assets/js/notification.js
@@ -1,4 +1,33 @@
-// Notification
+// "use strict";
+// const notification = document.querySelector(".notification i");
+// let notifyCount = document.querySelectorAll(".notify-count");
+
+// const iconLight = document.querySelector(".icon-light");
+// const iconLightActive = document.querySelectorAll(".icon-light.active");
+
+// let i = 0;
+// let checked = 0;
+
+// iconLight.addEventListener("click", () => {
+//   iconLight.classList.toggle("active");
+// });
+
+// function getCount() {
+//   for (i = 0; i < iconLightActive.length; i++) {
+//     checked += 1;
+//   }
+
+//   notifyCount[i].innerText = checked;
+
+//   console.log(`notifyCount: ${notifyCount.length}`);
+//   console.log(`iconLightActive.length: ${iconLightActive.length}`);
+//   console.log(`notifyCount[0].innerText: ${notifyCount[0].innerText}`);
+
+//   // notification.classList.toggle("active");
+// }
+
+// iconLight.addEventListener("click", getCount);
+
 const iconLight = document.querySelector(".icon-light");
 
 iconLight.addEventListener("click", () => {
